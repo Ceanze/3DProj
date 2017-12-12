4c0999cd26d55fb5e4ea025cc4798f63fd66c119
Jonathan Åleskog <jonathan9609082000@gmail.com>
2017-12-12 17:20:24 +0100

    Added "Hej"

--- 3DProj/3DProj/main.cpp
+++ 3DProj/3DProj/main.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
 #include <Windows.h>
 
-#include "Display.h"
 
 int main(void)
 {
-	Display display("Test window");
-	
-	glfwSetInputMode(display.getWindowPtr(), GLFW_STICKY_KEYS, GL_TRUE);
-	while (glfwGetKey(display.getWindowPtr(), GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(display.getWindowPtr()) == 0)
-	{
-		// Draw
-		glClearColor(0, 0, 0, 1);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Swap buffers
-		glfwSwapBuffers(display.getWindowPtr());
-		glfwPollEvents();
-	}
+	std::cout << "Hej" << std::endl;
 	return 0;
 }
