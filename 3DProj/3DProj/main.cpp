49641d976a557f191f44ec707be479de32e9d0d6
Simon Nylén <simnylen@gmail.com>
2017-12-12 16:29:51 +0100

    Added libraries

--- 3DProj/3DProj/main.cpp
+++ 3DProj/3DProj/main.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 #include <Windows.h>
 
-int main() 
+#include <gl\GL.h>
+#include <glfw3.h>
+#include <glm.hpp>
+
+int main(void)
 {
+	GLFWwindow* window;
 
-	std::cout << "ADRIAN & JONATHAN & SIMON >> Hello World!" << std::endl;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-	std::cout << "Hi Jonathan" << std::endl;
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-	getchar();
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
