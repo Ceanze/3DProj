bfba8c41a14ebeec79f0451ffe2ace5a2732a397
Adrian Nordin <adde--97@hotmail.com>
2017-12-15 15:33:00 +0100

    added state functionality

---
+++ 3DProj/3DProj/Core/States/GameState.cpp
@@ -0,0 +1,28 @@
+#include "GameState.h"
+
+
+GameState::GameState(Display * ptr = nullptr) : State(ptr)
+{
+
+}
+
+GameState::~GameState()
+{
+
+}
+
+void GameState::init()
+{
+}
+
+void GameState::update(float dt)
+{
+}
+
+void GameState::input()
+{
+}
+
+void GameState::render()
+{
+}
