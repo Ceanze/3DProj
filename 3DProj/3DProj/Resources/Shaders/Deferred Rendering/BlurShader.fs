fb39652424d3600da8405f74996bfc2d5096e204
Jonathan Åleskog <jonathan9609082000@gmail.com>
2018-01-30 14:54:56 +0100

    Fixed gaussian blur.

--- 3DProj/3DProj/Resources/Shaders/Deferred Rendering/BlurShader.fs
+++ 3DProj/3DProj/Resources/Shaders/Deferred Rendering/BlurShader.fs
@@ -8,18 +8,36 @@ uniform vec2 dir;
 
 out vec4 finalColor;
 
-const float blurArr[4] = float[4](0.002291, 0.023226, 0.092651, 0.146768);
+#define SIZE 3
+#define STANDARD_DEVIATION 1.5
+
+float gaussian(float x)
+{
+    const float ss2 = 2.0*STANDARD_DEVIATION*STANDARD_DEVIATION; 
+    return (1.0/sqrt(3.1415*ss2)) * exp(-(x*x)/ss2);
+}
+
+//const float blurArr[4] = float[4](0.002291, 0.023226, 0.092651, 0.146768);
 
 void main()
 {
+    float blurArr[SIZE+1];
+    float sum = 0.0;
+    for(int i = -SIZE; i <= 0; i++)
+    {
+        blurArr[i+SIZE] = gaussian(i);
+        sum += blurArr[i+SIZE]*(i==0?1:2);
+    }
     vec3 outColor = vec3(0.0, 0.0, 0.0);
     float hstep = 1.0/textureResolution.x;
     float vstep = 1.0/textureResolution.y;
     
-    for(int i = 0; i < 7; i++)
+    const int lineSize = SIZE*2+1;
+    for(int i = 0; i < lineSize; i++)
     {
-        vec2 uv = fragTextureCoord+vec2(hstep*(i-3)*dir.x, vstep*(i-3)*dir.y);
-        outColor += texture(finalTexture, uv).xyz*blurArr[i>3?6-i:i];
+        int halfSize = lineSize/2;
+        vec2 uv = fragTextureCoord+vec2(hstep*(i-halfSize)*dir.x, vstep*(i-halfSize)*dir.y);
+        outColor += texture(finalTexture, uv).xyz*blurArr[i>3?6-i:i]/sum;
     }
 
     finalColor = min(vec4(outColor, 1.0f), 1.0);
