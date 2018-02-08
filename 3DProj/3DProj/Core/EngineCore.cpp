c5db57da1a3f797e37c1ae3e768e9243f6fbf00d
Jonathan Åleskog <jonathan9609082000@gmail.com>
2018-02-08 15:01:51 +0100

    Merge

--- 3DProj/3DProj/Core/EngineCore.cpp
+++ 3DProj/3DProj/Core/EngineCore.cpp
@@ -32,7 +32,7 @@ EngineCore::EngineCore()
 	attachCamera(this->activeCamera);
 	
 	this->terrain.setShader(this->geometryShader);
-	this->base = new Entity({ 0.0f, 15.0f, -5.0f }, {0.0f, 0.0f, 0.0f});
+	this->base = new Entity({ 0.0f, 5.0f, -5.0f }, {0.0f, 0.0f, 0.0f});
 	this->base->addComponent(this->shadowCamera);
 	this->base->addComponent(new DirectionalLight(glm::normalize(glm::vec3(-3.0f, -5.0f, 1.0f)), 1.0f, glm::vec3(1.0f), this->deferredRenderer->getPhongShader()));
 
